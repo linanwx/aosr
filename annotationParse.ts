837ef90fda55f6cf4b0815430d665b0765f88f9d
linanwx <linanwx@gmail.com>
2022-12-02 17:37:21 +0800

    Optimize performance

--- annotationParse.ts
+++ annotationParse.ts
@@ -2,25 +2,35 @@ import { CardSchedule } from "schedule"
 import yaml from "yaml"
 
 export class AnnotationWrapper {
-    static defaultRegAnnotation = String.raw`%%[^\%\^]+?%%\n\^blockID`
     static defaultRegWrapper = /%%\n\`\`\`YAML\n([\s\S]+?)\`\`\`\n%%\n\^.+/gm
-    static findAnnotationWrapper(fileText:string, blockID:string) {
-        let regAnnotation = this.defaultRegAnnotation.replace("blockID", blockID)
-        let matchReg = new RegExp(regAnnotation, "gm")
-        let annotation = ""
-        fileText.match(matchReg)?.forEach((value) => {
-            annotation = value
-        })
-        return annotation
+    static findAnnotationWrapper(fileText: string, blockID: string): string {
+        let suffix = `\n^${blockID}`
+        let endpos = fileText.lastIndexOf(suffix)
+        if (endpos < 0) {
+            return ""
+        }
+        let startpos = fileText.lastIndexOf("%%", endpos)
+        if (startpos < 0) {
+            return ""
+        }
+        startpos = fileText.lastIndexOf("%%", startpos - 1)
+        if (startpos < 0) {
+            return ""
+        }
+        let annotationWrapper = fileText.substring(startpos, endpos + suffix.length)
+        if (!annotationWrapper.contains("```YAML")) {
+            return ""
+        }
+        return annotationWrapper
     }
-    static deWrapper(annotation:string):string {
+    static deWrapper(annotation: string): string {
         let results = annotation.matchAll(AnnotationWrapper.defaultRegWrapper)
         for (let result of results) {
             return result[1]
         }
         return ""
     }
-    static enWrapper(ID:string, annotation:string):string{
+    static enWrapper(ID: string, annotation: string): string {
         return "%%\n\`\`\`YAML\n" + annotation + "\`\`\`\n%%\n^" + ID
     }
 }
@@ -29,21 +39,21 @@ export class AnnotationObject {
     copy(obj: AnnotationObject) {
         this.cardSchedule.copy(obj.cardSchedule)
     }
-    cardSchedule:CardSchedule
+    cardSchedule: CardSchedule
     constructor() {
         this.cardSchedule = new CardSchedule()
     }
-    static Parse(annotation:string):AnnotationObject {
+    static Parse(annotation: string): AnnotationObject {
         if (!annotation) {
             return new AnnotationObject()
         }
         let ret = new AnnotationObject()
-        let obj:AnnotationObject = yaml.parse(annotation)
+        let obj: AnnotationObject = yaml.parse(annotation)
         ret.copy(obj)
         return ret
     }
-    static Stringify(fmt:AnnotationObject):string {
-        let s = yaml.stringify(fmt, (key, value)=> {
+    static Stringify(fmt: AnnotationObject): string {
+        let s = yaml.stringify(fmt, (key, value) => {
             if (value !== null) return value
         })
         return s
