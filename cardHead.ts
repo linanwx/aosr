422ca6518e52e3eaf8a234e2161ea1961b1487c7
linanwx <linanwx@gmail.com>
2023-06-16 11:57:37 +0800

    Optimize link display

--- cardHead.ts
+++ cardHead.ts
@@ -3,7 +3,7 @@ export const CardIDTag = "AOSR"
 // 更新卡片第一行的卡片ID
 export function UpdateCardIDTag(cardid:string, fileText:string, index:number):string {
     // let tag = " #" + CardIDTag + "/" + cardid
-    let tag = ` #${CardIDTag}/${cardid} [[#^${cardid}]]`
+    let tag = ` #${CardIDTag}/${cardid} [[#^${cardid}|🔗]]`
     for (let i=index;i<fileText.length;i++) {
         if (fileText[i] == "\n") {
             fileText = fileText.slice(0, i) + tag + fileText.slice(i)
